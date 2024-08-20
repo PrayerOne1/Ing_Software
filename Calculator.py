f30b700642a59ebb664d00d8ad120a7d6599b217
PrayerOne1 <160205286+PrayerOne1@users.noreply.github.com>
2024-08-20 12:17:50 -0600

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -32,8 +32,19 @@ def clear():
 
 # Crear la pantalla de la calculadora
 equation = tk.StringVar()
-screen = tk.Entry(root, textvariable=equation, font=('Arial', 20), bd=10, insertwidth=2, width=14, borderwidth=4)
-screen.grid(columnspan=4)
+screen = tk.Entry(root, textvariable=equation, font=('Arial', 20), bd=10, insertwidth=2, width=20, borderwidth=4)
+screen.grid(row=0, column=0, columnspan=5)
+
+# Crear labels para conversiones de numeros
+label_hex = tk.Label(root, font=('Arial', 14), text='Hex:')
+label_dec = tk.Label(root, font=('Arial', 14), text='Dec:')
+label_oct = tk.Label(root, font=('Arial', 14), text='Oct:')
+label_bin = tk.Label(root, font=('Arial', 14), text='Bin:')
+
+label_hex.grid(row=1, column=0, sticky='e')
+label_dec.grid(row=2, column=0, sticky='e')
+label_oct.grid(row=3, column=0, sticky='e')
+label_bin.grid(row=4, column=0, sticky='e')
 
 # Crear botones individuales
 btn1 = tk.Button(root, text='1', padx=20, pady=20, font=('Arial', 18), command=lambda: press_button(1))
@@ -47,34 +58,75 @@ btn8 = tk.Button(root, text='8', padx=20, pady=20, font=('Arial', 18), command=l
 btn9 = tk.Button(root, text='9', padx=20, pady=20, font=('Arial', 18), command=lambda: press_button(9))
 btn0 = tk.Button(root, text='0', padx=20, pady=20, font=('Arial', 18), command=lambda: press_button(0))
 
+# Botones para letras hexadecimales
+btnA = tk.Button(root, text='A', padx=20, pady=20, font=('Arial', 18))
+btnB = tk.Button(root, text='B', padx=20, pady=20, font=('Arial', 18))
+btnC = tk.Button(root, text='C', padx=20, pady=20, font=('Arial', 18))
+btnD = tk.Button(root, text='D', padx=20, pady=20, font=('Arial', 18))
+btnE = tk.Button(root, text='E', padx=20, pady=20, font=('Arial', 18))
+btnF = tk.Button(root, text='F', padx=20, pady=20, font=('Arial', 18))
+
+# Botones para operaciones matemáticas
 btn_add = tk.Button(root, text='+', padx=20, pady=20, font=('Arial', 18), command=lambda: press_button('+'))
 btn_sub = tk.Button(root, text='-', padx=20, pady=20, font=('Arial', 18), command=lambda: press_button('-'))
 btn_mul = tk.Button(root, text='*', padx=20, pady=20, font=('Arial', 18), command=lambda: press_button('*'))
 btn_div = tk.Button(root, text='/', padx=20, pady=20, font=('Arial', 18), command=lambda: press_button('/'))
 
+# Botones para funciones científicas
+btn_log = tk.Button(root, text='log', padx=20, pady=20, font=('Arial', 18))
+btn_por = tk.Button(root, text='%', padx=20, pady=20, font=('Arial', 18))
+btn_raiz = tk.Button(root, text='√', padx=20, pady=20, font=('Arial', 18))
+btn_fact = tk.Button(root, text='n!', padx=20, pady=20, font=('Arial', 18))
+btn_arri = tk.Button(root, text='^', padx=20, pady=20, font=('Arial', 18))
+btn_back = tk.Button(root, text='←', padx=20, pady=20, font=('Arial', 18))
+btn_abs = tk.Button(root, text='ABS', padx=20, pady=20, font=('Arial', 18))
+btn_deci = tk.Button(root, text='.', padx=20, pady=20, font=('Arial', 18))
+
 btn_equal = tk.Button(root, text='=', padx=20, pady=20, font=('Arial', 18), command=calculate)
-btn_clear = tk.Button(root, text='C', padx=20, pady=20, font=('Arial', 18), command=clear)
+btn_clear = tk.Button(root, text='CE', padx=20, pady=20, font=('Arial', 18), command=clear)
 
 # Organizar los botones en la cuadrícula
-btn7.grid(row=1, column=0)
-btn8.grid(row=1, column=1)
-btn9.grid(row=1, column=2)
-btn_div.grid(row=1, column=3)
+# Row = 1, column = n
+btnA.grid(row=5, column=0)
+btn_log.grid(row=5, column=1)
+btn_por.grid(row=5, column=2)
+btn_clear.grid(row=5, column=3)
+btn_back.grid(row=5, column=4)
 
-btn4.grid(row=2, column=0)
-btn5.grid(row=2, column=1)
-btn6.grid(row=2, column=2)
-btn_mul.grid(row=2, column=3)
+# Row = 2, column = n
+btnB.grid(row=6, column=0)
+btn_raiz.grid(row=6, column=1)
+btn_arri.grid(row=6, column=2)
+btn_fact.grid(row=6, column=3)
+btn_div.grid(row=6, column=4)
 
-btn1.grid(row=3, column=0)
-btn2.grid(row=3, column=1)
-btn3.grid(row=3, column=2)
-btn_sub.grid(row=3, column=3)
+# Row = 3, column = n
+btnC.grid(row=7, column=0)
+btn7.grid(row=7, column=1)
+btn8.grid(row=7, column=2)
+btn9.grid(row=7, column=3)
+btn_mul.grid(row=7, column=4)
 
-btn0.grid(row=4, column=0)
-btn_clear.grid(row=4, column=1)
-btn_equal.grid(row=4, column=2)
-btn_add.grid(row=4, column=3)
+# Row = 4, column = n
+btnD.grid(row=8, column=0)
+btn4.grid(row=8, column=1)
+btn5.grid(row=8, column=2)
+btn6.grid(row=8, column=3)
+btn_sub.grid(row=8, column=4)
+
+# Row = 5, column = n
+btnE.grid(row=9, column=0)
+btn1.grid(row=9, column=1)
+btn2.grid(row=9, column=2)
+btn3.grid(row=9, column=3)
+btn_add.grid(row=9, column=4)
+
+# Row = 6, column = n
+btnF.grid(row=10, column=0)
+btn_abs.grid(row=10, column=1)
+btn0.grid(row=10, column=2)
+btn_deci.grid(row=10, column=3)
+btn_equal.grid(row=10, column=4)
 
 # Ejecutar el loop principal de la GUI
 root.mainloop()
